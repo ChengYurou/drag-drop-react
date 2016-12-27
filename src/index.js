73b8b2e692b2a66517366fb2fac318a245c928ba
chengyurou <18829292695@163.com>
2016-12-27 21:00:55 +0800

    add list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {Component} from 'react'
 import ReactDom from 'react-dom';
 
+var colors = ["Red", "Green", "Blue", "Yellow", "Black", "White", "Orange"];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data
+    }
+  }
+
+  render() {
+    return (
+        <ul>
+          {this.props.data.map(item => <li>{item}</li>)}
+        </ul>
+    );
+  }
+}
+
 ReactDom.render(
-    <h1>Hello,World!</h1>,
+    <App data={colors}/>,
     document.getElementById('app')
 );
